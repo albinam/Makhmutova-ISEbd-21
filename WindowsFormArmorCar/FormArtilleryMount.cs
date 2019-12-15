f1a25e3dedab410c673c5237049cd26dd26bc19f
albinosikm <albinosikm@gmail.com>
2019-12-15 16:40:06 +0400

    update

--- WindowsFormArmorCar/FormArtilleryMount.cs
+++ WindowsFormArmorCar/FormArtilleryMount.cs
@@ -20,7 +20,7 @@ namespace WindowsFormArmorCar
         private void buttonSetArtilleryMount_Click(object sender, EventArgs e)
         {
             car = new ArtilleryMount(100, 300, Color.Green,
-           Color.Black, 20, true, false,Guns.Three);
+           Color.Black, 20, true, true);
             car.SetPosition(50, 50, pictureBoxArtilleryMount.Width,
            pictureBoxArtilleryMount.Height);
             Draw();
