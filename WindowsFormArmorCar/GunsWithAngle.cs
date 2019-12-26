375cf56522dd04aa9258bd952d7948b1116ec04b
albinosikm <albinosikm@gmail.com>
2019-12-26 12:30:27 +0400

    edit

--- WindowsFormArmorCar/GunsWithAngle.cs
+++ WindowsFormArmorCar/GunsWithAngle.cs
@@ -19,30 +19,30 @@ namespace WindowsFormArmorCar
         public void GunsDraw(Graphics g, Guns numberOfGuns, Color color)
         {
             int n = (int)numberOfGuns;
-            Brush br1 = new SolidBrush(color);
-            g.FillRectangle(br1, _startPosX + 22, _startPosY + 5, 47, 15);
-            Brush br2 = new SolidBrush(Color.Orange);
-            g.FillRectangle(br2, _startPosX + 25, _startPosY + 8, 25, 1);
-            g.FillRectangle(br2, _startPosX + 25, _startPosY + 10, 25, 1);
-            g.FillRectangle(br2, _startPosX + 25, _startPosY + 12, 25, 1);
-            Brush br3 = new SolidBrush(Color.Black);
-            g.FillRectangle(br3, _startPosX + 25, _startPosY + 9, 25, 1);
-            g.FillRectangle(br3, _startPosX + 25, _startPosY + 11, 25, 1);
-            Pen pen = new Pen(Color.Black);
+            Brush brush = new SolidBrush(color);
+            g.FillRectangle(brush, _startPosX + 22, _startPosY + 5, 47, 15);
+            Brush brushOrange = new SolidBrush(Color.Orange);
+            g.FillRectangle(brushOrange, _startPosX + 25, _startPosY + 8, 25, 1);
+            g.FillRectangle(brushOrange, _startPosX + 25, _startPosY + 10, 25, 1);
+            g.FillRectangle(brushOrange, _startPosX + 25, _startPosY + 12, 25, 1);
+            Brush brushBlack = new SolidBrush(Color.Black);
+            g.FillRectangle(brushBlack, _startPosX + 25, _startPosY + 9, 25, 1);
+            g.FillRectangle(brushBlack, _startPosX + 25, _startPosY + 11, 25, 1);
+            Pen penBlack = new Pen(Color.Black);
             if (n == 0)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
             }
             if (n == 1)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
             }
             if (n == 2)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 15, _startPosX + 89, _startPosY + 12);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 15, _startPosX + 89, _startPosY + 12);
             }
         }
     }
