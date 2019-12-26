fc20c3a077f352d84dbeb5715b1f5e1a79765c1c
albinosikm <albinosikm@gmail.com>
2019-12-26 12:29:17 +0400

    edit

--- WindowsFormArmorCar/SimpleGuns.cs
+++ WindowsFormArmorCar/SimpleGuns.cs
@@ -19,23 +19,23 @@ namespace WindowsFormArmorCar
         public void GunsDraw(Graphics g, Guns numberOfGuns,Color color)
         {        
             int n = (int) numberOfGuns;
-            Brush br1 = new SolidBrush(color);
-            g.FillRectangle(br1, _startPosX + 22, _startPosY + 5, 47, 15);
-            Pen pen = new Pen(Color.Black);
+            Brush brush = new SolidBrush(color);
+            g.FillRectangle(brush, _startPosX + 22, _startPosY + 5, 47, 15);
+            Pen penBlack = new Pen(Color.Black);
             if (n == 0)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
             }
             if (n == 1)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 9);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 9);
             }
             if (n== 2)
             {
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 9);
-                g.DrawLine(pen, _startPosX + 69, _startPosY + 15, _startPosX + 89, _startPosY + 15);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 12);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 9);
+                g.DrawLine(penBlack, _startPosX + 69, _startPosY + 15, _startPosX + 89, _startPosY + 15);
             }
         }
     }
